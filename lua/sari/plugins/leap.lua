457c21055f089e2ce21a8660e1ee21604ec30df5
Sari <whysoserious@Saris-MacBook-Air.local>
2026-07-16 13:16:01 +0300

    Small fixes and updates

--- lua/sari/plugins/leap.lua
+++ lua/sari/plugins/leap.lua
@@ -1,9 +1,36 @@
 return {
-    'ggandor/leap.nvim',
-    dependencies = {
-        'tpope/vim-repeat'
-    },
-    config = function()
-        require('leap').create_default_mappings()
-    end
+	"https://codeberg.org/andyg/leap.nvim",
+	dependencies = {
+		"tpope/vim-repeat",
+	},
+	config = function()
+		-- Highly recommended: define a preview filter to reduce visual noise
+		-- and the blinking effect after the first keypress.
+		-- For example, define word boundaries as the common case, that is, skip
+		-- preview for matches starting with whitespace or an alphabetic
+		-- mid-word character: foobar[baaz] = quux
+		--                     ^    ^^^  ^^ ^ ^  ^
+		require("leap").opts.preview = function(ch0, ch1, ch2)
+			return not (ch1:match("%s") or (ch0:match("%a") and ch1:match("%a") and ch2:match("%a")))
+		end
+
+		-- Enable the traversal keys to repeat the previous search without
+		-- explicitly invoking Leap (`<cr><cr>...` instead of `s<cr><cr>...`):
+		do
+			local clever = require("leap.user").with_traversal_keys
+			vim.keymap.set({ "n", "x", "o" }, "<cr>", function()
+				require("leap").leap({
+					["repeat"] = true,
+					opts = clever("<cr>", "<bs>"),
+				})
+			end)
+			vim.keymap.set({ "n", "x", "o" }, "<bs>", function()
+				require("leap").leap({
+					["repeat"] = true,
+					opts = clever("<bs>", "<cr>"),
+					backward = true,
+				})
+			end)
+		end
+	end,
 }
